190d0ecf69ca61a901ce125389cecea8b7aa71ac
Paul Cartwright <paulcartwright.74j@gmail.com>
2023-03-20 14:43:20 +0000

    Command line arguments add Converts PNG to JPG

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Merge GPX Routes and display traces on a map
 
 Packages Required:
   sudo apt-get install python3-matplotlib
-  sudo apt-get install python3-cartopy
   sudo apt-get install python3-pykdtree 
   sudo apt-get install python3-scipy
   sudo apt-get install python3-gpxpy
-  
+  sudo apt-get install libproj-dev libgeos-dev 
+  pip3 install cartopy
--- gpx_merge.py
+++ gpx_merge.py
@@ -7,52 +7,81 @@ import gpxpy
 import cartopy.crs as ccrs
 import cartopy.io.img_tiles as cimgt
 
-DEBUG = 0
+import argparse
+from PIL import Image
 
-request = cimgt.OSM()
+request = cimgt.OSM(cache='.')
 
-gpx_file = open('Silver2023PracticeGreenDay1.gpx', 'r')
+def parse_gpx_file (gpx_file):
+    gpx_file = open(gpx_file, 'r')
+    return gpxpy.parse(gpx_file)
 
-gpx = gpxpy.parse(gpx_file)
+def find_gpx_bbox(gpx,min_lat,min_lon,max_lat,max_lon):
+    lat_lon ={}
+    wp={}
+    
+    for waypoint in gpx.waypoints:
+        if DEBUG > 8: print('waypoint {0} -> ({1},{2})'.format(waypoint.name, waypoint.latitude, waypoint.longitude))
+        lat_lon['lat'] = waypoint.latitude 
+        lat_lon['lon'] = waypoint.longitude
+        wp[waypoint.name] = lat_lon 
+        if min_lat == "":
+            print("Min lat not set")
+            min_lat = waypoint.latitude
+            max_lat = waypoint.latitude
 
+            min_lon = waypoint.longitude
+            max_lon = waypoint.longitude
+        else:
+            if DEBUG >= 1: print("updat min/max")
+            min_lat = min(min_lat,waypoint.latitude)
+            max_lat = max(max_lat,waypoint.latitude)
 
-lat_lon ={}
-wp={}
+            min_lon = min(min_lon,waypoint.longitude)
+            max_lon = max(max_lon,waypoint.longitude)    
+
+    return [min_lat,min_lon,max_lat,max_lon]
+
+def plot_waypoints (gpx,colour):
+    if DEBUG >= 1: print("\nINFO: Start Plot waypoints\n")
+    flat=0
+    flon=0
+
+    for waypoint in gpx.waypoints:
+        if flat==0:
+            flat= waypoint.latitude
+            flon= waypoint.longitude
+        else:
+            pl.plot([flon, waypoint.longitude], [flat, waypoint.latitude], transform=ccrs.PlateCarree(), linewidth=1, color=colour, marker= '>', markersize=3)
+            flat= waypoint.latitude
+            flon= waypoint.longitude
+    if DEBUG >= 1: print("\nINFO: End Plot waypoints\n")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-g',  '--green')
+parser.add_argument('-y',  '--yellow')
+parser.add_argument('-p',  '--pink')
+parser.add_argument('-i',  '--purple')
+parser.add_argument('-gt', '--green_text')
+parser.add_argument('-yt', '--yellow_text')
+parser.add_argument('-pt', '--pink_text')
+parser.add_argument('-it', '--purple_text')
+parser.add_argument('-o',  '--out_file', required=True)
+parser.add_argument('-z',  '--zoom',default=15)
+parser.add_argument('-d',  '--debug',default=0)
+args = parser.parse_args()
+
+DEBUG = args.debug
+
+if args.green == None and args.yellow==None and args.pink==None and args.purple==None:
+    print("No gpx file supplied")
+    exit(1)
+
+   
 #for track in gpx.tracks:
 #    for segment in track.segments:
 #        for point in segment.points:
 #            print('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
-
-#<bounds minlat="50.9584770" minlon="-1.0271670" maxlat="50.9936480" maxlon="-0.9560900"/>
-
-min_lat = ""
-min_lon = ""
-max_lon = ""
-max_lon = ""
-#0.0
-for waypoint in gpx.waypoints:
-    if DEBUG > 8: print('waypoint {0} -> ({1},{2})'.format(waypoint.name, waypoint.latitude, waypoint.longitude))
-    lat_lon['lat'] = waypoint.latitude 
-    lat_lon['lon'] = waypoint.longitude
-    wp[waypoint.name] = lat_lon 
-    if min_lat == "":
-        min_lat = waypoint.latitude
-        max_lat = waypoint.latitude
-
-        min_lon = waypoint.longitude
-        max_lon = waypoint.longitude
-    else:
-        min_lat = min(min_lat,waypoint.latitude)
-        max_lat = max(max_lat,waypoint.latitude)
-
-        min_lon = min(min_lon,waypoint.longitude)
-        max_lon = max(max_lon,waypoint.longitude)
-    
-
-
-print(f'min_lon {min_lon:2.4}\t min_lat {min_lat:2.4}')         
-print(f'max_lon {max_lon:2.4}\t max_lat {max_lat:2.4}')    
-    
 
 #for route in gpx.routes:
 #    print('Route:')
@@ -60,49 +89,96 @@ print(f'max_lon {max_lon:2.4}\t max_lat {max_lat:2.4}')
 #        print('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
 
 
+if args.green is not None:
+    gpx_green = parse_gpx_file(args.green)
+    min_lat,min_lon,max_lat,max_lon = find_gpx_bbox(gpx_green,"","","","")
+
+if args.yellow is not None:
+    gpx_yellow = parse_gpx_file(args.yellow)
+    min_lat,min_lon,max_lat,max_lon = find_gpx_bbox(gpx_yellow,min_lat,min_lon,max_lat,max_lon)
+
+if args.pink is not None:
+    gpx_pink = parse_gpx_file(args.pink)
+    min_lat,min_lon,max_lat,max_lon = find_gpx_bbox(gpx_pink,min_lat,min_lon,max_lat,max_lon)
+
+if args.purple is not None:
+    gpx_purple = parse_gpx_file(args.purple)
+    min_lat,min_lon,max_lat,max_lon = find_gpx_bbox(gpx_pink,min_lat,min_lon,max_lat,max_lon)
+
+if DEBUG >= 1:
+    print(f'min_lon {min_lon}\t min_lat {min_lat}')         
+    print(f'max_lon {max_lon}\t max_lat {max_lat}')    
+    
+x=max_lon - min_lon
+y=max_lat - min_lat
 # Bounds: (lon_min, lon_max, lat_min, lat_max):
+
 extent = [(min_lon-0.0008), (max_lon+0.0008), (min_lat-0.0008), (max_lat+0.0008)]
 
-x= max_lon -min_lon
-y= max_lat -min_lat
-area = x*y
+A=4
+
+if x < y:
+    #Portrat
+    fig=pl.figure(figsize=[33.11 * 0.5**(0.5 * A), 46.82 * 0.5**(0.5 * A)], dpi=300)
+    #print(f'SIZE: {33.11 * 0.5**(0.5 * A)} , {46.82 * 0.5**(0.5 * A)}')
+else:
+    #Lanscape
+    fig=pl.figure(figsize=[46.82 * 0.5**(0.5 * A), 33.11 * 0.5**(0.5 * A)], dpi=300)
+    #print(f'SIZE: {46.82 * 0.5**(0.5 * A)} , {33.11 * 0.5**(0.5 * A)}')
 
 
-print(f'x={x} y={y} area={area}')
+#ax = pl.axes(projection=request.crs)
+ax = fig.add_subplot(1,1,1,projection=request.crs)
 
-
-
-#extent = [-0.9806340,-1.9806340 , 50.9638920,51.9638920]
-
-
-ax = pl.axes(projection=request.crs)
 ax.set_extent(extent)
-ax.add_image(request, 18)    # 5 = zoom level
+print("Retrieve Map")
+## 12	town, or city district
+## 13	village, or suburb
+## 14	thousand	
+## 15	small road
+## 16	street
+## 17	block, park, addresses
+## 18	some buildings, trees
+## 19	local highway and crossing details
+## 20	hundred	A mid-sized building
+ax.add_image(request, args.zoom)    # 5 = zoom level
 
+if args.green  is not None:plot_waypoints(gpx_green,'green')
+if args.yellow is not None:plot_waypoints(gpx_yellow,'yellow')
+if args.pink   is not None:plot_waypoints(gpx_pink,'pink')
+if args.purple is not None:plot_waypoints(gpx_purple,'purple')
 
-flat=0
-flon=0
-for waypoint in gpx.waypoints:
-    if flat==0:
-        flat= waypoint.latitude
-        flon= waypoint.longitude
-    else:
-        #print(f'{flon} {flat} {waypoint.longitude} {waypoint.latitude}')
-        pl.plot([flon, waypoint.longitude], [flat, waypoint.latitude], transform=ccrs.PlateCarree(), linewidth=1, color='green', marker= 'o', markersize=1)
-        flat= waypoint.latitude
-        flon= waypoint.longitude
-    #pl.scatter(waypoint.longitude, waypoint.latitude, transform=ccrs.PlateCarree(),c='blue')
 
 #pl.plot([min_lon, max_lon], [min_lat, max_lat], color='green', linewidth=2, marker='o', transform=ccrs.PlateCarree())
-
+offset=0
+if args.green_text  is not None:
+    pl.text(min_lon,min_lat+offset,args.green_text, transform=ccrs.PlateCarree())
+    offset=offset+0.001
+if args.yellow_text  is not None:
+    pl.text(min_lon,min_lat+0.001,args.yellow_text, transform=ccrs.PlateCarree())
+    offset=offset+0.001
+if args.pink_text  is not None:
+    pl.text(min_lon,min_lat+0.002,args.pink_text, transform=ccrs.PlateCarree())
+    offset=offset+0.001
+if args.purple_text  is not None:
+    pl.text(min_lon,min_lat+0.002,args.purple_text, transform=ccrs.PlateCarree())
+    offset=offset+0.001
+#([flon, waypoint.longitude], [flat, waypoint.latitude], transform=ccrs.PlateCarree(), linewidth=1, color=colour, marker= '>', markersize=3)
 
 
 print(f'[{min_lon}, {min_lat}], [{max_lon}, {max_lat}]')
 
 #pl.ax(figsize=(10,6))
 
-#pl.show()
-pl.savefig('map.png')
+if DEBUG >= 1: pl.show()
 
+fig.savefig(f'{args.out_file}.png')
+
+## convert png to jpg
+
+im = Image.open(f'{args.out_file}.png')
+im=im.convert('RGB')
+im.save(f'{args.out_file}.jpg', quality=95)
 
 print("\n\nEND\n\n")
+
